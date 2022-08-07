347f6ae911b7033e2eae6d2d0ed4836c79bc9252
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2022-08-07 08:45:25 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class test
+{
+   public static void main(String y[])
+   {
+      Scanner s=new Scanner(System.in);
+         System.out.println("enter a id");
+           int id=s.nextInt();
+         System.out.println("enter a name");
+           String name=s.next();
+         System.out.println("enter a sal");
+           Float sal=s.nextFloat();
+         System.out.println(id);
+         System.out.println(name);
+         System.out.println(sal);
+   }
+}
