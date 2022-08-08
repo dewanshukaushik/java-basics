567b45a6f29498de510cdbe401e679d1704bd61b
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2022-08-08 19:50:19 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,15 @@
+class demo
+{
+ public static void main(String s[])
+  {
+       int a=10;
+       byte b=5;
+       String str="ram";
+       float c=10.2f;
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(str);
+  System.out.println(c);
+  }
+}
+
